08958066f7135db21935851653811dfd7c6bb825
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2024-11-09 16:39:39 -0500

    fixes

--- feature/home/src/main/java/com/saico/cvportfolio/home/screen/MainCurriculumScreen.kt
+++ feature/home/src/main/java/com/saico/cvportfolio/home/screen/MainCurriculumScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
